52b5e598eea11d8adad85dda7273adecdccc4e86
Tudor Tocan <tudor.tocan@gmail.com>
2022-06-07 15:46:01 +0100

    add sign-in and signout methods

--- src/app/app-demo/app-demo.component.ts
+++ src/app/app-demo/app-demo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialAuthService } from "@abacritt/angularx-social-login";
+import {  GoogleLoginProvider } from "@abacritt/angularx-social-login";
+import { SocialUser } from "@abacritt/angularx-social-login";
 
 @Component({
   selector: 'app-app-demo',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-demo.component.css']
 })
 export class AppDemoComponent implements OnInit {
+  user: SocialUser;
+  loggedIn: boolean;
 
-  constructor() { }
+  constructor(private authService: SocialAuthService) { }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  refreshToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
 
   ngOnInit(): void {
+    console.log(this.loggedIn);
+    this.loggedIn = false
+    
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
   }
 
 }
