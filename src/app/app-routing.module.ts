0a472f397622c9effe410dc3144b9896cb119c08
Tudor Tocan <tudor.tocan@gmail.com>
2022-06-07 15:44:30 +0100

    add route to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppDemoComponent } from './app-demo/app-demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:AppDemoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
